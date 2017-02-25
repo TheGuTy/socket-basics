be52cb0e73e83eb7e566e0428a4440724b034539
Gabriel Gutierrez <Gabriel Gutierrez>
2017-02-25 17:01:20 -0300

    Added custom message event

--- server.js
+++ server.js
@@ -7,8 +7,17 @@ var io = require('socket.io')(http);
 app.use(express.static(__dirname + '/public'));
 
 // on() is going to let you listen for events
-io.on('connection', function () {
+io.on('connection', function (socket) {
   console.log('user connected via socket.io!');
+
+  socket.on('message', function (message) {
+    console.log('Message received: ' + message.text);
+    socket.broadcast.emit('message', message);  // send the message to everybody but the person who sends it
+  });
+
+  socket.emit('message', {
+    text: "Welcome to the chat application!"
+  });
 });
 
 http.listen(PORT, function () {
