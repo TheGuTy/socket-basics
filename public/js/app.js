403143296ffea94afff0dd9fcac8d7cd889ae830
Gabriel Gutierrez <nggutierrez.45@gmail.com>
2017-02-26 14:47:57 -0300

    Add name support

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,8 @@
+var name = getQueryVariable('name') || 'Annonymous';
+var room = getQueryVariable('room');
 var socket = io();
+
+console.log(name + ' wants to join ' + room);
 
 socket.on('connect', function () {
   console.log('Connected to socket.io server');
@@ -7,11 +11,13 @@ socket.on('connect', function () {
 socket.on('message', function (message) {
   //var momentTimestamp
   var momentTimestamp = moment.utc(message.timestamp);
+  var $message = jQuery('.messages');
 
   console.log('New Message:');
   console.log(message.text);
 
-  jQuery('.messages').append('<p><strong>' + momentTimestamp.local().format('hh:mm a') + '</strong>' + ': ' + message.text + '</p>');
+  $message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('hh:mm a') + '</strong></p´>')
+  $message.append('<p>' + message.text + '</p');
 });
 
 // Handles submitting of new message
@@ -23,6 +29,7 @@ $form.on('submit', function (event) {
   var $message = $form.find('input[name=message]');
 
 	socket.emit('message', {
+    name: name,
 		text: $message.val()
 	});
 
